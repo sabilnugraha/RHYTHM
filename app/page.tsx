986f9718089c7d8a44620aabdcdafe0c760498d4
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-04-25 07:09:06 +0700

    Add main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:20}}>
+      <h1>RHYTHM</h1>
+      <p>Intelligent ERP Platform - Adaptive Flow System</p>
+    </main>
+  )
+}
