37a1fefe9cb38e5ba1272f95991543ea75be468c
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-04-27 16:22:08 +0700

    Replace hero right panel with RHYTHM robot mascot

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,70 @@
+function RhythmBot() {
+  return (
+    <div className="relative mx-auto flex min-h-[520px] w-full max-w-[560px] items-end justify-center lg:min-h-[640px]">
+      <div className="absolute left-8 top-16 h-14 w-14 -rotate-6 border-2 border-black bg-[#E9D5FF] shadow-[5px_5px_0px_#000]" />
+      <div className="absolute right-6 top-10 h-16 w-16 rotate-3 border-2 border-black bg-[#FFD6A5] shadow-[5px_5px_0px_#000]" />
+      <div className="absolute bottom-16 left-3 h-12 w-12 rotate-6 border-2 border-black bg-[#CDEFD7] shadow-[5px_5px_0px_#000]" />
+
+      <div className="absolute left-6 top-24 hidden -rotate-2 rounded-[26px] border-2 border-black bg-[#FFFDF8] px-5 py-4 text-sm font-black shadow-[5px_5px_0px_#000] md:block">
+        I map.<br />
+        You flow.<br />
+        <span className="text-[#18A999]">We Rhythm.</span>
+      </div>
+
+      <div className="relative h-[500px] w-[390px] lg:h-[590px] lg:w-[450px]">
+        <div className="absolute left-1/2 top-0 h-16 w-2 -translate-x-1/2 border-2 border-black bg-[#18A999]" />
+        <div className="absolute left-1/2 top-0 h-11 w-11 -translate-x-1/2 rounded-full border-2 border-black bg-[#18A999] shadow-[4px_4px_0px_#000]" />
+
+        <div className="absolute left-1/2 top-12 h-56 w-72 -translate-x-1/2 rounded-[5rem] border-2 border-black bg-[#FFFDF8] shadow-[9px_9px_0px_#000] lg:h-64 lg:w-80">
+          <div className="absolute left-1/2 top-4 h-7 w-16 -translate-x-1/2 rounded-full border-2 border-black bg-[#18A999]" />
+          <div className="absolute -left-8 top-24 h-20 w-12 rounded-full border-2 border-black bg-[#18A999] shadow-[4px_4px_0px_#000]" />
+          <div className="absolute -right-8 top-24 h-20 w-12 rounded-full border-2 border-black bg-[#18A999] shadow-[4px_4px_0px_#000]" />
+          <div className="absolute left-1/2 top-16 flex h-28 w-56 -translate-x-1/2 items-center justify-center gap-12 rounded-[3rem] border-2 border-black bg-[#111827] shadow-inner lg:h-32 lg:w-64">
+            <div className="h-16 w-5 rounded-full bg-[#8EF5E5] shadow-[0_0_18px_#8EF5E5]" />
+            <div className="relative h-16 w-16">
+              <div className="absolute left-3 top-5 h-5 w-12 -rotate-45 rounded-full bg-[#8EF5E5] shadow-[0_0_18px_#8EF5E5]" />
+              <div className="absolute left-3 bottom-5 h-5 w-12 rotate-45 rounded-full bg-[#8EF5E5] shadow-[0_0_18px_#8EF5E5]" />
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute left-1/2 top-[250px] h-44 w-56 -translate-x-1/2 rounded-[4rem] border-2 border-black bg-[#FFFDF8] shadow-[8px_8px_0px_#000] lg:top-[290px] lg:h-52 lg:w-64">
+          <div className="absolute left-1/2 top-14 h-14 w-24 -translate-x-1/2">
+            <div className="absolute left-0 top-6 h-1 w-7 bg-[#18A999]" />
+            <div className="absolute left-7 top-2 h-1 w-5 rotate-[65deg] bg-[#18A999]" />
+            <div className="absolute left-11 top-8 h-1 w-5 -rotate-[65deg] bg-[#18A999]" />
+            <div className="absolute left-16 top-6 h-1 w-8 bg-[#18A999]" />
+          </div>
+        </div>
+
+        <div className="absolute left-4 top-[275px] h-28 w-20 -rotate-[35deg] rounded-full border-2 border-black bg-[#FFFDF8] shadow-[6px_6px_0px_#000] lg:left-0 lg:top-[305px]">
+          <div className="absolute -left-5 -top-4 h-16 w-16 rounded-full border-2 border-black bg-[#111827]">
+            <div className="absolute -right-6 top-2 h-6 w-12 rotate-[25deg] rounded-full border-2 border-black bg-[#111827]" />
+          </div>
+        </div>
+        <div className="absolute right-10 top-[298px] h-28 w-20 rotate-[18deg] rounded-full border-2 border-black bg-[#FFFDF8] shadow-[6px_6px_0px_#000] lg:right-8 lg:top-[335px]">
+          <div className="absolute -bottom-3 left-4 h-14 w-14 rounded-full border-2 border-black bg-[#111827]" />
+        </div>
+
+        <div className="absolute bottom-0 left-[82px] h-32 w-24 rounded-[2rem] border-2 border-black bg-[#FFFDF8] shadow-[6px_6px_0px_#000] lg:left-[92px]">
+          <div className="absolute bottom-0 left-0 h-8 w-full rounded-b-[2rem] bg-[#18A999]" />
+        </div>
+        <div className="absolute bottom-0 right-[82px] h-32 w-24 rounded-[2rem] border-2 border-black bg-[#FFFDF8] shadow-[6px_6px_0px_#000] lg:right-[92px]">
+          <div className="absolute bottom-0 left-0 h-8 w-full rounded-b-[2rem] bg-[#18A999]" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#F6F1E8] text-[#151515]">
       <section className="relative min-h-screen overflow-hidden px-6 py-6 md:px-10">
         <div className="absolute inset-0 opacity-70 [background-image:linear-gradient(#15151518_1px,transparent_1px),linear-gradient(90deg,#15151518_1px,transparent_1px)] [background-size:36px_36px]" />
-        <div className="absolute -left-20 top-4 h-36 w-36 border-2 border-black bg-[#BFE8FF] shadow-[7px_7px_0px_#000] -rotate-6" />
-        <div className="absolute right-12 top-28 h-16 w-16 border-2 border-black bg-[#FFD6A5] shadow-[6px_6px_0px_#000] rotate-3" />
-        <div className="absolute -right-12 bottom-20 h-40 w-40 border-2 border-black bg-[#CDEFD7] shadow-[7px_7px_0px_#000] -rotate-6" />
+        <div className="absolute -left-20 top-4 h-36 w-36 -rotate-6 border-2 border-black bg-[#BFE8FF] shadow-[7px_7px_0px_#000]" />
+        <div className="absolute right-12 top-28 h-16 w-16 rotate-3 border-2 border-black bg-[#FFD6A5] shadow-[6px_6px_0px_#000]" />
+        <div className="absolute -right-12 bottom-20 h-40 w-40 -rotate-6 border-2 border-black bg-[#CDEFD7] shadow-[7px_7px_0px_#000]" />
 
         <div className="relative z-10 mx-auto max-w-7xl">
           <nav className="flex items-center justify-between border-2 border-black bg-[#FFFDF8] px-5 py-4 shadow-[6px_6px_0px_#000]">
@@ -37,23 +96,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="relative">
-              <div className="absolute -left-4 -top-4 h-full w-full border-2 border-black bg-[#E9D5FF] shadow-[8px_8px_0px_#000]" />
-              <div className="relative border-2 border-black bg-[#FFFDF8] p-5 shadow-[8px_8px_0px_#000]">
-                <div className="mb-4 flex items-center justify-between border-b-2 border-black pb-4">
-                  <div>
-                    <p className="text-xs font-black uppercase tracking-[0.2em] text-[#18A999]">Live System Map</p>
-                    <h2 className="mt-1 text-xl font-black uppercase">Business Flow</h2>
-                  </div>
-                  <div className="border-2 border-black bg-[#CDEFD7] px-3 py-2 text-xs font-black shadow-[3px_3px_0px_#000]">ONLINE</div>
-                </div>
-                <div className="space-y-3">
-                  <div className="border-2 border-black bg-[#F6F1E8] p-3 shadow-[4px_4px_0px_#000]"><div className="text-xs font-black text-[#18A999]">01</div><div className="mt-1 text-lg font-black uppercase">Structure Layer</div><div className="mt-1 text-sm text-neutral-700">Objects and fields</div></div>
-                  <div className="border-2 border-black bg-[#F6F1E8] p-3 shadow-[4px_4px_0px_#000]"><div className="text-xs font-black text-[#18A999]">02</div><div className="mt-1 text-lg font-black uppercase">Control Layer</div><div className="mt-1 text-sm text-neutral-700">Rules and workflows</div></div>
-                  <div className="border-2 border-black bg-[#F6F1E8] p-3 shadow-[4px_4px_0px_#000]"><div className="text-xs font-black text-[#18A999]">03</div><div className="mt-1 text-lg font-black uppercase">Operational Layer</div><div className="mt-1 text-sm text-neutral-700">Daily execution</div></div>
-                </div>
-              </div>
-            </div>
+            <RhythmBot />
           </div>
         </div>
       </section>
