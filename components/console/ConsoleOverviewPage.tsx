b7fca5df82d17cda867d55446477770f68c81644
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-05-04 06:29:28 +0700

    Add console overview component

---
+++ components/console/ConsoleOverviewPage.tsx
@@ -0,0 +1,6 @@
+import { ConsoleShell } from './layout/ConsoleShell';
+import { OverviewContent } from './overview/OverviewContent';
+
+export function ConsoleOverviewPage() {
+  return <ConsoleShell><OverviewContent /></ConsoleShell>;
+}
