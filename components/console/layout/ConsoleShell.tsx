4a788b7d4a0daa874ee5fdfcfe719745ef2846b4
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-05-04 06:59:07 +0700

    Soften console shell surfaces

--- components/console/layout/ConsoleShell.tsx
+++ components/console/layout/ConsoleShell.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react';
 
 import type { ConsoleTab } from '../ConsoleOverviewPage';
-import { ConsoleSidebar } from './ConsoleSidebar';
-import { ConsoleTopBar } from './ConsoleTopBar';
-import { ConsoleTabBar } from './ConsoleTabBar';
 import { RhythmDock } from '../dock/RhythmDock';
+import { ConsoleSidebar } from './ConsoleSidebar';
+import { ConsoleTabBar } from './ConsoleTabBar';
+import { ConsoleTopBar } from './ConsoleTopBar';
 
 type ConsoleShellProps = {
   children: ReactNode;
@@ -18,14 +18,14 @@ type ConsoleShellProps = {
 
 export function ConsoleShell({ children, tabs, activeTabId, activeSection, onOpenTab, onSelectTab, onCloseTab }: ConsoleShellProps) {
   return (
-    <main className="min-h-screen overflow-hidden bg-[#F6F1E8] text-[#151515]">
-      <div className="absolute inset-0 opacity-60 [background-image:linear-gradient(#15151514_1px,transparent_1px),linear-gradient(90deg,#15151514_1px,transparent_1px)] [background-size:32px_32px]" />
+    <main className="min-h-screen overflow-hidden bg-[#F4EFE7] text-[#151515]">
+      <div className="absolute inset-0 opacity-50 [background-image:linear-gradient(#15151510_1px,transparent_1px),linear-gradient(90deg,#15151510_1px,transparent_1px)] [background-size:32px_32px]" />
       <div className="relative z-10 flex min-h-screen">
         <ConsoleSidebar activeSection={activeSection} onOpenTab={onOpenTab} />
         <section className="flex min-w-0 flex-1 flex-col p-3">
           <ConsoleTopBar />
           <ConsoleTabBar tabs={tabs} activeTabId={activeTabId} onSelectTab={onSelectTab} onCloseTab={onCloseTab} />
-          <div className="min-h-0 flex-1 overflow-auto border-2 border-black bg-[#FFFDF8] p-4 shadow-[4px_4px_0px_#000]">
+          <div className="min-h-0 flex-1 overflow-auto rounded-b-3xl rounded-tr-3xl border border-black/20 bg-[#FFFDF8] p-4 shadow-[0_18px_45px_rgba(20,20,20,0.10)]">
             {children}
           </div>
         </section>
