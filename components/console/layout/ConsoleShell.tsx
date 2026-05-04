c602f50c845eb59c5139b7403b15810ab8f9922b
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-05-04 20:44:06 +0700

    Refine console shell spacing and density

--- components/console/layout/ConsoleShell.tsx
+++ components/console/layout/ConsoleShell.tsx
@@ -29,22 +29,21 @@ export function ConsoleShell({
   onSelectTab,
   onCloseTab,
 }: ConsoleShellProps) {
-  const shellTheme = isDarkMode ? 'bg-[#09090B] text-[#FFF7E8]' : 'bg-[#F7F0E3] text-[#101014]';
-  const contentTheme = isDarkMode ? 'border-black bg-[#15151A] shadow-[8px_8px_0px_#000]' : 'border-black bg-[#FFFDF8] shadow-[8px_8px_0px_#000]';
+  const shellTheme = isDarkMode ? 'bg-[#090A0F] text-[#F8F5EE]' : 'bg-[#F5F0E7] text-[#121216]';
+  const contentTheme = isDarkMode ? 'border-black bg-[#15161D] shadow-[4px_4px_0px_#000]' : 'border-black bg-[#FFFDF8] shadow-[4px_4px_0px_#000]';
 
   return (
     <main className={`min-h-screen overflow-hidden ${shellTheme}`}>
-      <div className="absolute inset-0 opacity-45 [background-image:linear-gradient(#00000018_1px,transparent_1px),linear-gradient(90deg,#00000018_1px,transparent_1px)] [background-size:32px_32px]" />
-      <div className="pointer-events-none absolute -left-12 top-28 h-36 w-36 rotate-12 rounded-[34px] border-[3px] border-black bg-[#00D4FF] shadow-[8px_8px_0px_#000]" />
-      <div className="pointer-events-none absolute right-8 top-24 h-20 w-20 rounded-full border-[3px] border-black bg-[#A3FF12] shadow-[7px_7px_0px_#000]" />
-      <div className="pointer-events-none absolute bottom-10 right-72 h-24 w-24 -rotate-12 border-[3px] border-black bg-[#FF4FB8] shadow-[8px_8px_0px_#000]" />
+      <div className="absolute inset-0 opacity-25 [background-image:linear-gradient(#00000014_1px,transparent_1px),linear-gradient(90deg,#00000014_1px,transparent_1px)] [background-size:32px_32px]" />
+      <div className="pointer-events-none absolute right-8 top-24 h-14 w-14 rounded-full border-2 border-black bg-[#A3FF12] shadow-[4px_4px_0px_#000]" />
+      <div className="pointer-events-none absolute bottom-8 right-72 h-16 w-16 -rotate-12 border-2 border-black bg-[#FF4FB8] shadow-[4px_4px_0px_#000]" />
 
       <div className="relative z-10 flex min-h-screen">
         <ConsoleSidebar activeSection={activeSection} isDarkMode={isDarkMode} onOpenTab={onOpenTab} />
-        <section className="flex min-w-0 flex-1 flex-col p-3">
+        <section className="flex min-w-0 flex-1 flex-col p-3 pl-0">
           <ConsoleTopBar isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
           <ConsoleTabBar tabs={tabs} activeTabId={activeTabId} isDarkMode={isDarkMode} onSelectTab={onSelectTab} onCloseTab={onCloseTab} />
-          <div className={`min-h-0 flex-1 overflow-auto rounded-b-[28px] rounded-tr-[28px] border-[3px] p-4 ${contentTheme}`}>{children}</div>
+          <div className={`min-h-0 flex-1 overflow-auto rounded-b-2xl rounded-tr-2xl border-2 p-3 ${contentTheme}`}>{children}</div>
         </section>
       </div>
       <RhythmDock />
