bafe0d5884f39e142f96c2489109dd3fd38167fd
Sabilly Nugraha <101251168+sabilnugraha@users.noreply.github.com>
2026-05-05 17:22:40 +0700

    Remove content padding for Package Studio

--- components/console/layout/ConsoleShell.tsx
+++ components/console/layout/ConsoleShell.tsx
@@ -30,6 +30,7 @@ export function ConsoleShell({
 }: ConsoleShellProps) {
   const shellTheme = isDarkMode ? 'bg-[#080A0F] text-[#F7F2E8]' : 'bg-[#F6F1E8] text-[#111216]';
   const contentTheme = isDarkMode ? 'border-black bg-[#141720] shadow-[3px_3px_0px_#000]' : 'border-black bg-[#FFFDF8] shadow-[3px_3px_0px_#000]';
+  const contentPadding = activeSection === 'Package Studio' ? 'p-0' : 'p-2.5';
 
   return (
     <main className={`h-screen overflow-hidden ${shellTheme}`}>
@@ -40,7 +41,7 @@ export function ConsoleShell({
         <ConsoleSidebar activeSection={activeSection} isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} onOpenTab={onOpenTab} />
         <section className="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden p-2 pl-0 pt-0">
           <ConsoleTabBar tabs={tabs} activeTabId={activeTabId} isDarkMode={isDarkMode} onSelectTab={onSelectTab} onCloseTab={onCloseTab} />
-          <div className={`console-scrollbar ${isDarkMode ? 'console-scrollbar-dark' : ''} min-h-0 flex-1 overflow-auto rounded-b-xl rounded-tr-xl border-2 p-2.5 ${contentTheme}`}>{children}</div>
+          <div className={`console-scrollbar ${isDarkMode ? 'console-scrollbar-dark' : ''} min-h-0 flex-1 overflow-auto rounded-b-xl rounded-tr-xl border-2 ${contentPadding} ${contentTheme}`}>{children}</div>
         </section>
       </div>
       <RhythmDock />
